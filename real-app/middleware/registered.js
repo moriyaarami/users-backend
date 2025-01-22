396dc8874f3e092a5bc2c8983bcfa79cc559b78e
moriya <moriyastudent@gmail.com>
2025-01-22 23:09:47 +0200

    added

--- real-app/middleware/registered.js
+++ real-app/middleware/registered.js
@@ -2,14 +2,12 @@ const { logRequest } = require("../logFunction");
 const { User } = require("../schema/users");
 
 module.exports = async (req, res, next) => {
-    console.log(req.user._id)
 
     const user = await User.findById(req.user._id);
 
-
     if (!user) {
-        logRequest(401, "not found user with is id.")
-        return res.status(401).send("not found user with is id.");
+        logRequest(401, "not found user with this id.")
+        return res.status(401).send("not found user with this id.");
     }
     const user_id = req.user._id;
     const paramId = req.params.id;
